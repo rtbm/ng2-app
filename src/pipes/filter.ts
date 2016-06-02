e53a26cf9ac85bd17fe53f769b6cf3eda3fc425a
rtbm <marcin@bogusz.cc>
2016-06-03 00:19:41 +0200

    filter pipe refactor

--- src/pipes/filter.ts
+++ src/pipes/filter.ts
@@ -7,12 +7,18 @@ export class FilterPipe implements PipeTransform {
     const regexp = new RegExp(phrase, 'i');
 
     return items.filter(item => {
-      if (!phrase) { return true; }
-      if (!keys.length) { return item.match(regexp); }
+      if (!phrase) {
+        return true;
+      }
+
+      if (!keys.length) {
+        return item.match(regexp);
+      }
 
       for (let n = keys.length - 1; n !== -1; n--) {
-        const key = keys[n];
-        if (item[key].match(regexp)) { return true; }
+        if (item[keys[n]].match(regexp)) {
+          return true;
+        }
       }
 
       return false;
