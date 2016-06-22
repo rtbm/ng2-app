773052df63f1c07df0c80eaa1a9bb9b593f6b4d3
m <marcin@bogusz.cc>
2016-06-22 14:07:17 +0200

    signin test

--- test/auth.js
+++ test/auth.js
@@ -15,7 +15,7 @@ describe('/api/auth', () => {
   });
 
   describe('/api/auth/signup', () => {
-    it('should return validation error status', done => {
+    it('should send validation error status', done => {
       chai.request(server)
         .post('/api/auth/signup')
         .send({ email: 'test@test', password: '1', password_confirm: '2' })
@@ -40,7 +40,7 @@ describe('/api/auth', () => {
         });
     });
 
-    it('should NOT add a user and return duplicated status', done => {
+    it('should NOT add a user and send duplicated status', done => {
       chai.request(server)
         .post('/api/auth/signup')
         .send({ email: 'test@test', password: 'test', password_confirm: 'test' })
@@ -54,6 +54,42 @@ describe('/api/auth', () => {
             });
         });
     });
+  });
 
+  describe('/api/auth/signin', () => {
+    it('should authenticate user and send JWT token', done => {
+      chai.request(server)
+        .post('/api/auth/signup')
+        .send({ email: 'test@test', password: 'test', password_confirm: 'test' })
+        .end((err, res) => {
+          chai.request(server)
+            .post('/api/auth/signin')
+            .send({ email: 'test@test', password: 'test' })
+            .end((err, res) => {
+              res.should.have.a.status(200);
+              res.should.be.a.json;
+              res.body.should.be.a('object');
+              res.body.should.have.property('id_token');
+              res.body.id_token.should.be.a.string;
+              res.body.id_token.should.be.not.empty;
+              done();
+            });
+        });
+    });
+
+    it('should NOT authenticate user', done => {
+      chai.request(server)
+        .post('/api/auth/signup')
+        .send({ email: 'test@test', password: 'test', password_confirm: 'test' })
+        .end((err, res) => {
+          chai.request(server)
+            .post('/api/auth/signin')
+            .send({ email: 'test@test', password: 'test2' })
+            .end((err, res) => {
+              res.should.have.a.status(422);
+              done();
+            });
+        });
+    });
   });
 });
