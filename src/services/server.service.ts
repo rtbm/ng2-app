a7f5f4fb2c099262c831391ff43a72739d5e3c05
rtbm <marcin@bogusz.cc>
2016-07-30 19:09:20 +0200

    server service update

--- src/services/server.service.ts
+++ src/services/server.service.ts
@@ -4,7 +4,7 @@ import { Http, Response, Headers } from '@angular/http';
 @Injectable()
 export class ServerService {
   private HEADERS = new Headers({ 'Content-Type': 'application/json' });
-  private API = `${__BASE_URL__}/api`;
+  private API_URL = `${__BASE_URL__}/api`;
 
   constructor(private http: Http) {
   }
@@ -22,28 +22,28 @@ export class ServerService {
   post(path, data) {
     this.setHeaders();
 
-    return this.http.post(`${this.API}${path}`, JSON.stringify(data), { headers: this.HEADERS })
+    return this.http.post(`${this.API_URL}${path}`, JSON.stringify(data), { headers: this.HEADERS })
       .map((res: Response) => res.json());
   }
 
   get(path) {
     this.setHeaders();
 
-    return this.http.get(`${this.API}${path}`, { headers: this.HEADERS })
+    return this.http.get(`${this.API_URL}${path}`, { headers: this.HEADERS })
       .map((res: Response) => res.json());
   }
 
   put(path, data) {
     this.setHeaders();
 
-    return this.http.put(`${this.API}${path}`, JSON.stringify(data), { headers: this.HEADERS })
+    return this.http.put(`${this.API_URL}${path}`, JSON.stringify(data), { headers: this.HEADERS })
       .map((res: Response) => res.json());
   }
 
   delete(path) {
     this.setHeaders();
 
-    return this.http.delete(`${this.API}${path}`, { headers: this.HEADERS })
+    return this.http.delete(`${this.API_URL}${path}`, { headers: this.HEADERS })
       .map((res: Response) => res.json());
   }
 }
